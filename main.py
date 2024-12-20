30eb1d22ab06306b3a606b88411af70a7686634e
emirhanerbil <emirhan117net@gmail.com>
2024-12-20 16:28:11 +0300

    test

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from fastapi import FastAPI
 import json
-from vllm import LLM, SamplingParams
+# from vllm import LLM, SamplingParams
 
-# Modeli yükle
-model_name="Qwen/Qwen2-7B-Instruct"
-# Initialize the model with vLLM
-llm = LLM(
-    model=model_name,
-    trust_remote_code=True,  # Required for Qwen models
-    tensor_parallel_size=1,   # Adjust based on available GPUs
-    dtype="float16",         # Using float16 for better memory efficiency
-    gpu_memory_utilization=0.8,
-    max_model_len=5608
-)
+# # Modeli yükle
+# model_name="Qwen/Qwen2-7B-Instruct"
+# # Initialize the model with vLLM
+# llm = LLM(
+#     model=model_name,
+#     trust_remote_code=True,  # Required for Qwen models
+#     tensor_parallel_size=1,   # Adjust based on available GPUs
+#     dtype="float16",         # Using float16 for better memory efficiency
+#     gpu_memory_utilization=0.8,
+#     max_model_len=5608
+# )
 
 
 app = FastAPI()
@@ -42,15 +42,15 @@ async def get_user(q: str,cid: str):
         cat_list = data[cid]
         prompt = format_chat_prompt(q, cat_list)
 
-        sampling_params = SamplingParams(
-            temperature=0.2,
-            top_p=0.9,
-            max_tokens=64,
-            stop=["\n"]
-            )
-        prompt = format_chat_prompt(q, cat_list)
-        outputs = llm.generate([prompt], sampling_params)
-        response = outputs[0].outputs[0].text.strip()
-        return {"status": "success", "data": q,"cid":cid,"res":response}  
+        # sampling_params = SamplingParams(
+        #     temperature=0.2,
+        #     top_p=0.9,
+        #     max_tokens=64,
+        #     stop=["\n"]
+        #     )
+        # prompt = format_chat_prompt(q, cat_list)
+        # outputs = llm.generate([prompt], sampling_params)
+        # response = outputs[0].outputs[0].text.strip()
+        return {"status": "success", "data": q,"cid":cid,"res":"response"}  
     except Exception as e:
         return {"status": "error","error_msg":str(e), "res": ""}
